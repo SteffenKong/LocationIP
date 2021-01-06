76d83333fdf5e7705164d1c3ad5b258c287293fd
konghy <konghy@pvc123.com>
2021-01-06 10:39:25 +0800

    fix(main): 去掉信息

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ use SteffenKong\LocationIP\LocationIP;
 require './vendor/autoload.php';
 
 try {
-    $json = (LocationIP::instance('787e80c32f40cc6ad4f529dccbd5e51f'))->setHttpClientOptions([
+    $json = (LocationIP::instance(''))->setHttpClientOptions([
         'timeout' => 2.0,
-    ])->getLocation('61.145.213.26');
+    ])->getLocation('');
     var_dump($json);
 }catch (\Exception $e)
 {
